7c9a0d9ee0b8ac60d4eb49e717a0ad89173eb63f
Alexander <seclen09@gmail.com>
2024-11-19 15:24:11 -0500

    Actualización - alexander

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,8 +30,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call(UsertypesSeeder::class);
         $this->call(UserSeeder::class);
-        
-        $this->call(VehicleSeeder::class);              // crear el procedure: sp_vehicle()
 
+        $this->call(VehicleSeeder::class);              // crear el procedure: sp_vehicles()
+        $this->call(ZoneSeeder::class);              // crear el procedure: sp_zones()
     }
 }
---
+++ database/seeders/ZoneSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ZoneSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::unprepared('
+        CREATE PROCEDURE `recicla_usat`.`sp_zones`(
+            IN _operacion INT,
+            IN _id BIGINT
+        )
+        BEGIN
+            IF _operacion = 1 THEN
+                SELECT z.id AS id, z.name AS name, z.area AS area, s.name AS sector, z.description AS description
+                FROM zones z
+                INNER JOIN sectors s ON z.sector_id = s.id;
+            END IF;
+
+            IF _operacion = 2 THEN
+                SELECT z.id AS id, z.name AS name, z.area AS area, s.name AS sector, z.description AS description
+                FROM zones z
+                INNER JOIN sectors s ON z.sector_id = s.id
+                WHERE z.id = _id;
+            END IF;
+        
+            IF _operacion = 3 THEN
+                SELECT z.name as zone, z2.latitude, z2.longitude 
+                FROM zones z 
+                INNER JOIN zonecoords z2 ON z2.zone_id = z.id 
+                WHERE z.sector_id = _id;
+            END IF;
+            
+        END
+
+    ');
+    }
+}
